9ec847836f32bbc62b39780d1eed4666babfc3ed
nlvrodnie <rlecera@netlinkvoice.com>
2023-04-23 07:39:05 +0800

    update

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -6,7 +6,7 @@
             <vue-qrcode value="Hello, World!" :options="{ width: 400 }"></vue-qrcode>
         </div> -->
 
-        <div class="h-screen flex w-full justify-center items-center ">
+        <div class="h-screen flex w-full justify-center items-center bg-red-900">
             <div class="flex gap-3">
                 <button class="py-2 px-6 text-white bg-blue-700 rounded-full"><router-link to="/login">Login</router-link></button>
                 <button class="py-2 px-6 text-white bg-green-700 rounded-full"><router-link to="/register">Get QR Code</router-link></button>
