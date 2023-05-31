66d46357ee5539ef8bb29c4b284d3eec446173af
nlvrodnie <rlecera@netlinkvoice.com>
2023-06-01 06:32:35 +0800

    update

--- src/Pages/ScannerPage.vue
+++ src/Pages/ScannerPage.vue
@@ -6,11 +6,12 @@ const events = ref([]);
 const currentEvent = ref(null);
 const loading = ref(false);
 let form = reactive({});
+let category = ref("timeIn");
 
 const getEvents = async () => {
   let item = await useApi().get("/events");
   let user = await useApi().get("/user");
-  console.log('user',user)
+  console.log("user", user);
   events.value = item.data;
 };
 
@@ -18,7 +19,7 @@ onMounted(() => {
   getEvents();
 });
 
-const onDecode = async (id) => {
+const timeIn = async (id) => {
   try {
     loading.value = true;
     form.student_id = Number(id);
@@ -29,20 +30,33 @@ const onDecode = async (id) => {
   } finally {
     loading.value = false;
   }
-  // alert(`this is sample value ${text} , the event ${currentEvent.value}`);
-  // console.log(`Decode text from QR code is ${text}`);
+
+};
+
+
+const timeOut = async (id) => {
+  try {
+    loading.value = true;
+    form.student_id = Number(id);
+    form.event_id = currentEvent;
+    await useApi().post("/setTimeOut", form);
+  } catch (error) {
+    alert("Error while scanning please try again.");
+  } finally {
+    loading.value = false;
+  }
+
 };
 const onLoaded = () => {
   console.log(`Ready to start scanning barcodes`);
 };
 
 const logout = async () => {
-  await useApi().post('/logout')
-  localStorage.removeItem('isLoggedIn');
-  localStorage.removeItem('token');
-  router.push('/')
-
-}
+  await useApi().post("/logout");
+  localStorage.removeItem("isLoggedIn");
+  localStorage.removeItem("token");
+  router.push("/");
+};
 </script>
 <template>
   <div>
@@ -56,22 +70,56 @@ const logout = async () => {
           </option>
         </select>
       </div>
-      <div v-if="loading">Scanning...</div>
-      <div class="px-4" v-else>
-        <StreamBarcodeReader
-          @decode="onDecode"
-          @loaded="onLoaded"
-        ></StreamBarcodeReader>
+      <div>
+        <div v-if="category == 'timeIn'">
+          <div v-if="loading">Scanning...</div>
+          <div class="px-4" v-else>
+            <StreamBarcodeReader
+              @decode="timeIn"
+              @loaded="onLoaded"
+            ></StreamBarcodeReader>
+          </div>
+        </div>
+        <div v-if="category == 'timeOut'">
+          <div v-if="loading">Scanning...</div>
+          <div class="px-4" v-else>
+            <StreamBarcodeReader
+              @decode="timeOut"
+              @loaded="onLoaded"
+            ></StreamBarcodeReader>
+          </div>
+        </div>
+
+        <div class="flex items-center gap-4 w-full justify-center pt-8">
+          <button
+            :class="{ '!bg-blue-700': category == 'timeIn' }"
+            class="p-2 px-4 rounded-full bg-gray-400 text-white"
+            @click="category = 'timeIn'"
+          >
+            Time In
+          </button>
+          <button
+          :class="{ '!bg-blue-700 ': category == 'timeOut' }"
+            class=" p-2 px-4 rounded-full bg-gray-400 text-white"
+            @click="category = 'timeOut'"
+          >
+            time Out
+          </button>
+        </div>
       </div>
 
-      <div class="absolute bottom-0 w-full"><button class="w-full p-2  bg-green-700 text-white" @click="logout">Logout</button></div>
+      <div class="absolute bottom-0 w-full">
+        <button class="w-full p-2 bg-green-700 text-white" @click="logout">
+          Logout
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { StreamBarcodeReader } from "vue-barcode-reader";
-import router from '@/router';
+import router from "@/router";
 
 export default {
   components: {
